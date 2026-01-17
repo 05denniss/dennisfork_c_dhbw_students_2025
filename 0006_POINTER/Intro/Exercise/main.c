46d5e40c683913df460917091c748bbd86bc3f1f
05denniss <nik.dennis.05@gmail.com>
2026-01-17 20:05:25 +0100

    0006result

--- 0006_POINTER/Intro/Exercise/main.c
+++ 0006_POINTER/Intro/Exercise/main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-    // YOUR CODE HERE //
+
+    int aValue = 42;
+    int *myPointer = NULL;
+    myPointer = &aValue;
+
+    printf("%d\n", aValue);
+    printf("%d\n", *myPointer);
+
+//    *myPointer = 20;
+
+    printf("%d\n", *myPointer);
+
+    int **yourPointer = NULL;
+    yourPointer = &myPointer;
+//    **yourPointer = 10;
+
+    printf("%d\n", **yourPointer);
+    printf("\n");
+    printf("%p\n", (void *)&aValue);
+    printf("%p\n", (void *)myPointer);
+    printf("%p\n", (void *)&myPointer);
+    printf("%p\n", (void *)yourPointer);  
+    printf("%p\n", (void *)&yourPointer);    
 
     return 0;
 }
