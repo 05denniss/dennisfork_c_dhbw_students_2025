3783031a49f8aec606191205f6c68c711e36c9cb
05denniss <nik.dennis.05@gmail.com>
2026-01-17 14:45:52 +0100

    task0002.1result

--- 0002_VARIABLES/Math_Computations/Exercise/main.c
+++ 0002_VARIABLES/Math_Computations/Exercise/main.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
 
-int main()
-{
 
-    return 0;
+double z1(int x, int y) {
+    double result = (x*x+y*y-(x*y)+2);
+    return result;
+}
+
+double z2(int x, int y) {
+    double result = ((x-y)*(x-y)*(x-y)-3);
+    return result;
+}
+
+double z3(int x, int y) {
+    double result = ((2*(x*x*x)-0.5*(x*x)-x+4)/y);
+    return result;
+}
+
+int main(void) {
+    int x = -2;
+    int y = -3;
+
+    double resultz1 = z1(x,y);
+    double resultz2 = z2(x,y);    
+    double resultz3 = z3(x,y);  
+
+    printf("z1 = %.6f \n", resultz1);
+    printf("z2 = %.6f \n", resultz2);
+    printf("z3 = %.6f \n", resultz3);
 }
 
 
