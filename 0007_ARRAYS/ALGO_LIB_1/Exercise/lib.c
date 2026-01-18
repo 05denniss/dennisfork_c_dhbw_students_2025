96db25356118f7f8154ecb2c493a3b9a37535f7a
05denniss <nik.dennis.05@gmail.com>
2026-01-18 18:07:44 +0100

    task0007.1result

--- 0007_ARRAYS/ALGO_LIB_1/Exercise/lib.c
+++ 0007_ARRAYS/ALGO_LIB_1/Exercise/lib.c
@@ -1,20 +1,56 @@
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stddef.h>
 
 #include "lib.h"
 
+
 bool allOf(int32_t *array, size_t length, int32_t value)
 {
-}
+    int i;
+
+    for (i=0; length>i; i++) {
+        if (array[i] != value) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 bool anyOf(int32_t *array, size_t length, int32_t value)
 {
-}
+    int i;
+
+    for (i=0; length>i; i++) {
+        if (array[i] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 bool noneOf(int32_t *array, size_t length, int32_t value)
 {
-}
+    int i;
+
+    for (i=0; length>i; i++) {
+        if (array[i] == value) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 size_t count(int32_t *array, size_t length, int32_t value)
 {
+    int i;
+    int countnum = 0;
+
+    for (i=0; length>i; i++) {
+        if (array[i] == value) {
+            countnum++;
+        }
+    }
+    return countnum;
 }
