b3a5f6ae2f2c2d2955839c5c21f3a795ab399b47
05denniss <nik.dennis.05@gmail.com>
2026-01-19 15:33:09 +0100

    task12result

--- 0012_VARIADIC_FUNCTIONS/INTRO/main.c
+++ 0012_VARIADIC_FUNCTIONS/INTRO/main.c
@@ -3,10 +3,27 @@
 
 // Function for calculating the sum of a variable number of arguments
 
-int main()
+int calcSumVariadicFunc(int count, ...) {
+    va_list args;
+    int tempsum = 0;
+
+    va_start(args, count);
+
+    for (int i = 0; i < count; i++)
+    {
+        tempsum += va_arg(args, int);
+    }
+
+    va_end(args);
+    
+    return tempsum;
+}
+
+int main(void)
 {
     printf("Sum of 1, 2, 3 --> %d\n", calcSumVariadicFunc(3, 1, 2, 3)); // Expected output: 6
     printf("Sum of 4, 5, 6, 7, 11, 12, 13 --> %d\n", calcSumVariadicFunc(7, 4, 5, 6, 7, 11, 12, 13));
+    printf("Sum of 5, 10, 15, 20, 30 --> %d\n", calcSumVariadicFunc(5, 10, 15, 20, 30, 40, 50, 60));
 
     return 0;
 }
